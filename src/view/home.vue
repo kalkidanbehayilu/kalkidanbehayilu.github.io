18b44475a1c095f6a99dcd3094d003d88f81b529
kalbehayilu <kalkidan.behayilu@hypedriven.com>
2022-04-18 11:42:16 +0300

    Added coming soon gif

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,11 +1,13 @@
 <template>
-  <img class="w-full" :src="logo" />
+  <div class="flex justify-center h-screen bg-black">
+    <img class="max-w-xs max-h-64 m-auto"  :src="logo" />
+  </div>
 </template>
 
 <script>
 // import Header from "../components/layout/header/header";
 // import Footer from "../components/layout/footer/footer";
-import logo from "../../assets/gif/coming-soon.gif";
+import logo from "../assets/gif/coming-soon.gif";
 export default {
   name: "HomePage",
   components: {
