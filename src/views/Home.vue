f44d829a7ee1725fc3831f5f54cb27dac9c1678e
kalkidanbehayilu <kalkidanbehayilu@gmail.com>
2025-03-05 09:47:35 +0300

    Remove about

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,5 @@
 <template>
 	<div class="container mx-auto">
-		<!-- Banner -->
-		<AppBanner class="mb-5 sm:mb-8" />
-
-		<!-- About page -->
-		<About />
-
 		<!-- Blogs -->
 		<BlogsGrid />
 
@@ -20,18 +14,14 @@
 </template>
 
 <script>
-import AppBanner from '@/components/shared/AppBanner';
 import BlogsGrid from '../components/blogs/BlogsGrid.vue';
-import About from "@/components/about/About.vue";
 import AboutFrameWorks from "../components/tech/FrameWorks.vue";
 import GitRepo from "../components/git/GitRepo.vue";
 
 export default {
 	name: 'Home',
 	components: {
-		AppBanner,
 		BlogsGrid,
-		About,
 		AboutFrameWorks,
 		GitRepo
 	},
