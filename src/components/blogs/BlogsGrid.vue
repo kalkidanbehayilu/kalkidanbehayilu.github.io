f76f53fa25ca2fa421cd00eb5760114b9b4b3296
kalkidanbehayilu <kalkidanbehayilu@gmail.com>
2025-03-05 11:56:36 +0300

    Remove blog title

--- src/components/blogs/BlogsGrid.vue
+++ src/components/blogs/BlogsGrid.vue
@@ -1,14 +1,6 @@
 <template>
 	<!-- Blogs grid -->
 	<section class="pt-10 sm:pt-14">
-		<!-- Blogs grid title -->
-		<div class="text-center">
-			<p
-				class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light">
-				{{ blogsHeading }}
-			</p>
-		</div>
-
 		<!-- Blogs grid -->
 		<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10">
 			<BlogSingle v-for="blog in filteredBlogs" :key="blog.id" :blog="blog" />
